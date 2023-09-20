6c3fa4a12dbf72775a4928a969e8e6564f64ceb4
Simonas Riska <simonas.riska@outlook.com>
2023-09-20 23:57:37 +0300

    Simplify Currency model by removing Name property

--- currency-converter-api/Models/Currency.cs
+++ currency-converter-api/Models/Currency.cs
@@ -3,6 +3,5 @@
     public class Currency
     {
         public string Code { get; set; }
-        public string Name { get; set; }
     }
 }
--- currency-converter-app/src/app/models/currency.model.ts
+++ currency-converter-app/src/app/models/currency.model.ts
@@ -1,4 +1,3 @@
 export class Currency {
     code: string = '';
-    name: string = '';
 }
